67c0e09f65084d01a4983bf5a17c3a44053ac456
GuzelGalimzyanova <galimzyanovagr@gmail.com>
2022-12-02 17:50:03 +0300

    Task 3

---
+++ Lesson1/Task3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+    Console.WriteLine("Это число четное");
+else
+    Console.WriteLine("Это число нечетное");
