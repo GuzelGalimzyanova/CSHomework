ff2520cdf9a61fee4d8ddd15168296c80c220ab9
GuzelGalimzyanova <galimzyanovagr@gmail.com>
2022-12-02 16:52:39 +0300

    Task 1

---
+++ Lesson1/Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b > a)
+    Console.WriteLine("max = " + b + ", min = " + a);
+else
+    Console.WriteLine("max = " + a + ", min = " + b);
+
