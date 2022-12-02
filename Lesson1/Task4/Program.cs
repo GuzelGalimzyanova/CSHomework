9c0fb506ca44bccf02d42d70cb3400c97dc80671
GuzelGalimzyanova <galimzyanovagr@gmail.com>
2022-12-02 18:15:27 +0300

    Task4

---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число больше единицы");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = 0;
+if (n < 1)
+{
+    Console.WriteLine("Ошибка ввода");
+    return;
+}
+while (a + 2 <= n)
+{
+    Console.Write(a + 2 + " ");
+    a = a + 2;
+}
