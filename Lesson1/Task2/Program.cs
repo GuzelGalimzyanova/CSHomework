e2afd02652d6e72413b76fefc6ae30775d923695
GuzelGalimzyanova <galimzyanovagr@gmail.com>
2022-12-02 17:36:39 +0300

    Task2

---
+++ Lesson1/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max && b > c)
+    max = b;
+if (c > max && c > b)
+    max = c;
+if (b < max && c < max)
+    max = a;
+Console.WriteLine("Максимальное число равно "+max);
+
+
+
