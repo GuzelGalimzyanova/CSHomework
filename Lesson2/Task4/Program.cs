b3f56d239407ab8e709cf807cc19cb3d658fe16b
GuzelGalimzyanova <galimzyanovagr@gmail.com>
2022-12-04 18:15:02 +0300

    Task 4

---
+++ Lesson2/Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a/b==b)
+    Console.WriteLine("Первое число является квадратом второго");
+else if (b/a==a)
+    Console.WriteLine("Второе число является квадратом первого");
+    else
+    Console.WriteLine("Числа не являются квадратами");
