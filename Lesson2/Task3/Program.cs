b02249af9607f4586403709c9caf24a07519fe63
GuzelGalimzyanova <galimzyanovagr@gmail.com>
2022-12-04 17:58:27 +0300

    Task 3

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число от 1 до 7");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == 6 || num == 7)
+    Console.WriteLine("Этот день является выходным");
+else if (num >= 1 && num <= 5)
+    Console.WriteLine("Этот день является будним");
+else
+    Console.WriteLine("Ошибка ввода");
