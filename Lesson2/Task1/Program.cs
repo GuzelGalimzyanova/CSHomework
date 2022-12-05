e7b5cbde03909ef1f1849ef4ce1b14d391c370be
GuzelGalimzyanova <galimzyanovagr@gmail.com>
2022-12-05 11:50:08 +0300

    Task2

--- Lesson2/Task1/Program.cs
+++ Lesson2/Task1/Program.cs
@@ -1,3 +1,15 @@
 ﻿Console.WriteLine("Введите трехзначное число ");
-string number = Console.ReadLine();
-Console.WriteLine("Вторая цифра = " + number[1]);
+string input = Console.ReadLine();
+
+if (number.Length == 3)
+    Console.WriteLine("Вторая цифра = " + number[1]);
+else
+    Console.WriteLine("Ошибка ввода ");
+
+
+//1- числа трехзн полож
+//2-числ не трехзн полож
+//3- не числа, текст
+//4- отрицательное трехзн число
+//5- отриц не трехзн число
+//6- дробные числа
--- Lesson2/Task2/Program.cs
+++ Lesson2/Task2/Program.cs
@@ -1,6 +1,23 @@
-﻿Console.WriteLine("Введите число ");
+﻿/*Console.WriteLine("Введите число ");
 string number = Console.ReadLine();
 if (number.Length < 3)
     Console.WriteLine("Третьей цифры нет ");
 else
-    Console.WriteLine("Третья цифра = " + number[2]);
+    Console.WriteLine("Третья цифра = " + number[2]);*/
+
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 0)
+    num = -num;
+if (num > 99)
+{
+    int a = num % 10;
+    string b = a.ToString();
+    Console.WriteLine("Третья цифра = " + b[0]);
+}
+else
+    Console.WriteLine("Третьей цифры нет ");
+
+
+
+
