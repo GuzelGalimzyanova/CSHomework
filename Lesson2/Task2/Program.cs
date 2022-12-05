e8d306e06ec6b98e06aef27c089fe1f63a01cbd0
GuzelGalimzyanova <galimzyanovagr@gmail.com>
2022-12-05 12:05:29 +0300

    T2

--- Lesson2/Task2/Program.cs
+++ Lesson2/Task2/Program.cs
@@ -6,18 +6,24 @@ else
     Console.WriteLine("Третья цифра = " + number[2]);*/
 
 Console.WriteLine("Введите число ");
-int num = Convert.ToInt32(Console.ReadLine());
-if (num < 0)
-    num = -num;
-if (num > 99)
+string input = Console.ReadLine();
+if (isNumeric(input))
 {
-    int a = num % 10;
-    string b = a.ToString();
-    Console.WriteLine("Третья цифра = " + b[0]);
+    int num = Convert.ToInt32(input);
+    if (num < 0)
+        num = -num;
+    if (num > 99)
+    {
+        int a = num % 10;
+        string b = a.ToString();
+        Console.WriteLine("Третья цифра = " + b[0]);
+    }
+    else
+        Console.WriteLine("Третьей цифры нет ");
 }
 else
-    Console.WriteLine("Третьей цифры нет ");
-
-
-
-
+Console.WriteLine("Ошибка ввода");
+bool isNumeric(string s)
+{
+    return int.TryParse(s, out int n);
+}
