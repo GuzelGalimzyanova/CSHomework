4c310a53a51f55aaa5054235e4be814a382f2049
GuzelGalimzyanova <galimzyanovagr@gmail.com>
2022-12-06 17:49:48 +0300

    Task 3

--- Lesson3/Task2/Program.cs
+++ Lesson3/Task2/Program.cs
@@ -1,6 +1,7 @@
 ﻿//Напишите программу, которая принимает на вход координаты двух точек 
 //и находит расстояние между ними в 3D пространстве.
 // points[0]-x1, points[1]-y1, points[2]-z1, points[3]-x2, points[4]-y2, points[5]-z2
+Console.WriteLine("Введите координаты");
 int[] points = new int[6];
 for (int i = 0; i < points.Length; i++)
 {
--- Lesson3/Task3/Program.cs
+++ Lesson3/Task3/Program.cs
@@ -3,5 +3,5 @@ Console.WriteLine("Введите число ");
 int n = Convert.ToInt32(Console.ReadLine());
 for (int i = 1; i <= n; i++)
 {
-    Console.Write(i*i*i+" ");
+    Console.Write(i*i*i +" ");
 }
