0a4c6fc06a46206a83009ad90528a596e491d838
GuzelGalimzyanova <galimzyanovagr@gmail.com>
2022-12-06 17:07:10 +0300

    Task 2

---
+++ Lesson3/Task2/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек 
+//и находит расстояние между ними в 3D пространстве.
+// points[0]-x1, points[1]-y1, points[2]-z1, points[3]-x2, points[4]-y2, points[5]-z2
+int[] points = new int[6];
+for (int i = 0; i < points.Length; i++)
+{
+    points[i] = Convert.ToInt32(Console.ReadLine());
+}
+double D = Math.Sqrt((points[3] - points[0]) * (points[3] - points[0]) + (points[4] - points[1]) * (points[4] - points[1]) + (points[5] - points[2]) * (points[5] - points[2]));
+Console.WriteLine("Растояние = " + Math.Round(D, 2));
